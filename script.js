34d2256dd2c6d04f1a9f9a98103121b3d5e6a0fa
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-11 19:12:21 +0300

    Finished working on the script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const form = document.getElementById('registrationform');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault();
+
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        const isValid = true;
+        const messages = [];
+
+        if(username.length < 3){
+            isValid = false;
+            messages.push('Username must be at lease 3 characters long!');
+        }
+
+        if(!(email.includes('@') && email.includes('.'))){
+            isValid = false;
+            messages.push('Email must have @ and .');
+        }
+
+        if(password.length < 8){
+            isValid = false;
+            messages.push('Passwrod must be atlease 8 characters long!');
+        }
+
+        feedbackDiv.style.display = 'block';
+        if(isValid){
+            feedbackDiv.textContent = 'Registration successful!';
+            feedbackDiv.style.color = '#28a745';
+        } else{
+            feedbackDiv.innerHTML = messages.join('<br>');
+            feedbackDiv.style.color = '#dc3545';
+        }
+
+        form.submit();
+    });
+});
