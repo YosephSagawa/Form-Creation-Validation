34c64a9a1ecca9d145c3da02035133f7cfc6f59b
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-11 19:18:34 +0300

    Added the function keyword to the DOMContentLoaded

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', function(){
     const form = document.getElementById('registrationform');
     const feedbackDiv = document.getElementById('form-feedback');
 
